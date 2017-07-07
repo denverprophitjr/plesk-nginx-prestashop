40a9cd9cc3169beaf74ff09f4337561c415b4799
Denver Prophit Jr <denver.prophit@gmail.com>
2017-07-07 08:42:05 -0400

    new files

--- opt/psa/admin/conf/templates/domain/service/fpm.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @var Template_VariableAccessor $VAR
- */
-?>
-        fastcgi_split_path_info ^((?U).+\.php)(/?.+)$;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
-        fastcgi_pass "<?php echo $VAR->domain->physicalHosting->fpmSocket ?>";
-        include /etc/nginx/fastcgi.conf;
