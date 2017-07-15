500d9fb09eb3ab063ca03f4faa4cf26afcf3cef5
Denver Prophit Jr <denver.prophit@gmail.com>
2017-07-15 10:55:44 -0400

    SSL Hardening
    Signed-off-by: Denver Prophit Jr <denver.prophit@gmail.com>

--- opt/psa/admin/conf/templates/custom/webmail/webmail.php
+++ opt/psa/admin/conf/templates/custom/webmail/webmail.php
@@ -69,7 +69,7 @@ SSLStaplingCache shmcb:/var/run/ocsp(128000)
         SSLUseStapling on
         SSLStaplingResponderTimeout 5
         SSLStaplingReturnResponderErrors off
-        add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" preload; always;
+        Header always set Strict-Transport-Security "max-age=63072000; includeSubdomains; preload;"
     <?php endif; ?>
     <?php endif; ?>
 
